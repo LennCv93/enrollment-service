871324b00c0ec0d5fb8ee47cb49e5bc945551f1d
Lennin Cubas <valcubaslr@gmail.com>
2026-07-08 18:31:45 -0500

    Initial commit

---
+++ src/test/java/pe/tecsup/project/lms/enrollment_service/EnrollmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.tecsup.project.lms.enrollment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
